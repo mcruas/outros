fcb58b1a7f8be4fe937c8275865fe02632afa8e4
mcruas <mcruas@gmail.com>
2014-04-08 10:39:59 -0300

    Create instalador bugigangas linux.sh

---
+++ instalador bugigangas linux.sh
@@ -0,0 +1,8 @@
+%Comandos iniciais linux
+%=========================
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get install python-software-properties 
+sudo apt-add-repository ppa:libreoffice/libreoffice-4-2
+sudo apt-get update
+sudo apt-get install grub-customizer libreoffice r-base chromium-browser git synaptic gedit gedit-r-plugin
