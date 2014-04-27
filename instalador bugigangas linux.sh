a3ae1058838e98dd9ea335ecd43e410e2552f865
mcruas <mcruas@gmail.com>
2014-04-27 12:00:31 -0300

    Update instalador bugigangas linux.sh

--- instalador bugigangas linux.sh
+++ instalador bugigangas linux.sh
@@ -5,4 +5,7 @@ sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 sudo apt-get install python-software-properties 
 sudo apt-add-repository ppa:libreoffice/libreoffice-4-2
 sudo apt-get update
-sudo apt-get install grub-customizer libreoffice r-base chromium-browser git synaptic gedit gedit-r-plugin
+sudo apt-get install grub-customizer libreoffice chromium-browser \
+git synaptic gedit synapse nautilus-dropbox \
+r-base gedit-r-plugin \
+ipython ipython-notebook spyder
