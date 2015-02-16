9789c2e95579607634d96f9fbe7f92e8e56a4e19
mcruas <mcruas@gmail.com>
2015-02-16 17:03:07 -0200

    Update instalador bugigangas linux.sh

--- instalador bugigangas linux.sh
+++ instalador bugigangas linux.sh
@@ -9,5 +9,10 @@ sudo apt-add-repository ppa:libreoffice/libreoffice-4-2
 %Colocar o julia 
 sudo apt-add-repository ppa:staticfloat/juliareleases
 
+%R na versão atualizada para ubuntu
+sudo echo "\ndeb http://cran.fiocruz.br/bin/linux/ubuntu trusty/\n" >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+
 sudo apt-get update
-sudo apt-get install grub-customizer libreoffice chromium-browser git synaptic gedit synapse nautilus-dropbox r-base gedit-r-plugin ipython ipython-notebook spyder 
+sudo apt-get install grub-customizer libreoffice chromium-browser git synaptic gedit synapse nautilus-dropbox r-base ipython ipython-notebook spyder terminator qbittorrent
+
