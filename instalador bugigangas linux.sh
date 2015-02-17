fce6ef9e6391dd269bb79fa8dac7e76449de95c0
mcruas <mcruas@gmail.com>
2015-02-17 12:26:21 -0200

    Update instalador bugigangas linux.sh

--- instalador bugigangas linux.sh
+++ instalador bugigangas linux.sh
@@ -13,6 +13,19 @@ sudo apt-add-repository ppa:staticfloat/juliareleases
 sudo echo "\ndeb http://cran.fiocruz.br/bin/linux/ubuntu trusty/\n" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 
-sudo apt-get update
-sudo apt-get install grub-customizer libreoffice chromium-browser git synaptic gedit synapse nautilus-dropbox r-base ipython ipython-notebook spyder terminator qbittorrent
+% Repositório com synapse e youtube-dl
 
+
+sudo apt-get update
+sudo apt-get install grub-customizer libreoffice chromium-browser git synaptic gedit synapse nautilus-dropbox r-base ipython ipython-notebook spyder terminator qbittorrent youtube-dl
+
+%setar git
+git config --global user.email "mcruas@gmail.com"
+git config --global user.name "Marcelo Ruas"
+
+
+% Script inicialização
+%======================
+xfce4-power-manager &
+tilt2 &
+dropbox start &
